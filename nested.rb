13f341ca42c937a55be12f2638f06f895d282e03
Ian McKenzie <imckenzie777@gmail.com>
2020-05-01 12:15:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,6 @@ def changing_alan
         :languages => ["C"]
       }
     }
-#programmer_hash[:alan_kay][:known_for] = [:known_for => "GUI"]
 programmer_hash = {:alan_kay => {:known_for => "GUI"}}
 end
 
