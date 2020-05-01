1199e2ea8d1f9217c316910cab13fcad1fefcc19
Ian McKenzie <imckenzie777@gmail.com>
2020-05-01 12:28:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
     }
 #
 #programmer_hash = {:dennis_ritchie => {:languages => "Assembly"[1]}}
-programmer_hash[:dennis_ritchie][:languages][1]"Assembly"
+programmer_hash[:dennis_ritchie][:languages][1] = Assembly.to_s
 end
