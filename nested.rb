4e2461154f20a7de6cca22a657e96ec745df29d6
Ian McKenzie <imckenzie777@gmail.com>
2020-05-01 12:06:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-puts programmer_hash[:dennis_ritchie] = [:languages => "Assembly"]
+puts programmer_hash[:dennis_ritchie][:languages => "Assembly"] = [1]
 end
